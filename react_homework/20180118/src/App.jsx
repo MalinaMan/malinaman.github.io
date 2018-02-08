6554c111a3c4596524ad497fb2d0dda8e4cbd98d
MalinaMan <nick.malinovskiy@gmail.com>
2018-02-08 16:52:48 +0200

    some improve

--- react_homework/20180118/src/App.jsx
+++ react_homework/20180118/src/App.jsx
@@ -3,9 +3,6 @@ import { Link } from 'react-router';
 import PropTypes from 'prop-types';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
-import injectTapEventPlugin from 'react-tap-event-plugin';
-injectTapEventPlugin();
-
 import './styles/base.less';
 
 export default class App extends React.Component {
--- react_homework/20180118/src/stores/SessionStore.js
+++ react_homework/20180118/src/stores/SessionStore.js
@@ -7,23 +7,24 @@ const CHANGE_EVENT = 'change';
 
 let _isLoggedIn = false;
 
-const SessionStore = Object.assign({}, EventEmitter.prototype, {
-    isLoggedIn() {
-        return _isLoggedIn;
-    },
+const SessionStore = {...EventEmitter.prototype, ...{
+        isLoggedIn() {
+            return _isLoggedIn;
+        },
 
-    emitChange() {
-        this.emit(CHANGE_EVENT);
-    },
+        emitChange() {
+            this.emit(CHANGE_EVENT);
+        },
 
-    addChangeListener(callback) {
-        this.on(CHANGE_EVENT, callback);
-    },
+        addChangeListener(callback) {
+            this.on(CHANGE_EVENT, callback);
+        },
 
-    removeChangeListener(callback) {
-        this.removeListener(CHANGE_EVENT, callback);
+        removeChangeListener(callback) {
+            this.removeListener(CHANGE_EVENT, callback);
+        }
     }
-});
+};
 
 AppDispatcher.register(action => {
     console.info(action.type, action);
